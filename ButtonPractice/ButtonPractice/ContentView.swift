0362e31f36c3292b63da723f66b05ad6ccab2594
Chirag Tailor <chirag.tailor@thecoventry.co.uk>
2024-02-14 11:51:48 +0000

    Updated Buttons.

--- ButtonPractice/ButtonPractice/ContentView.swift
+++ ButtonPractice/ButtonPractice/ContentView.swift
@@ -9,51 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            
-            
+        VStack {
             Button("Primary") {
                 print("press me ")
                 
             }
             .font(.title)
             .padding(.all, 14)
-            .background(Color.blue)
+            .background(Color("cbsBlue"))
             .foregroundColor(.white)
             .cornerRadius(25)
             
-        }
-        Button("Primary") {
-            print("pressed")
+            Button("Primary") {
+                print("pressed")
+                
+            }
+            .font(.title)
+            .padding(.all, 14)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(25)
             
+            Button("Secondary") {
+                print("pressed")
+            }
+            .padding(.all)
+            .foregroundColor(.white)
+            .background(Color.black)
+            .cornerRadius(25)
+            .overlay(
+                Capsule(style: .continuous)
+                    .stroke(Color.blue, lineWidth: 2))
+            
+            
+            Button("Secondary") {
+                print("Pressed")
+            }
+            .padding(.all)
+            .foregroundColor(.black)
+            .background(Color.white)
+            //.border(Color.blue, width: 3)
+            .cornerRadius(4)
+            .overlay(
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(Color.blue, lineWidth: 3))
         }
-        .font(.title)
-        .padding(.all, 14)
-        .background(Color.blue)
-        .foregroundColor(.white)
-        .cornerRadius(25)
-        
     }
-     Button( "Secondary") {
-        print("pressed")
-    }
-      .padding(.all)
-    .foregroundColor(.white)
-    .background(Color.black)
-    .overlay(
-    Capsule(style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
-        .stroke(Color.blue, lineWidth: 2))
-    
+}
 
-        Button("Secondary") {
-            print("Pressed")
-        }
-        .padding(.all)
-        .foregroundColor(.black)
-        .background(Color.white)
-        //.border(Color.blue, width: 3)
-        .overlay(
-        RoundedRectangle(cornerRadius: 25)
-            .stroke(Color.blue, lineWidth: 3))
-    
+#Preview {
+    ContentView()
 }
